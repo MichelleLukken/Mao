b20c31621a89d1da5772883d0005e2eda748424b
Anette Habanen <habanen@ut.ee>
2021-03-22 10:43:22 +0200

    Lauas mingid asjad

--- src/Laud.java
+++ src/Laud.java
@@ -1,2 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Laud {
+
+    private Kaardipakk käidudKaardid;
+
+    public boolean mänguAlustamine(Kaardipakk kaardipakk){
+
+        for (int i = 0, j = 51; i < 5; i++, j--) {
+            int suvalineIndeks = (int) (Math.random()*j);
+            ArrayList<Kaart> suvalineKaart = kaardipakk.getPakk(suvalineIndeks); //See väljastab kaardipaki
+            System.out.println(suvalineIndeks);
+            System.out.println(suvalineKaart);
+            // add kaart kätte
+            // eemalda kaart pakist?
+            // jätab indeksid meelde, et need on pakist ära võetud
+        }
+        for (int i = 0, j = 46; i < 5; i++, j--) {
+            int suvalineIndeks = (int) (Math.random()*j);
+            ArrayList<Kaart> suvalineKaart = kaardipakk.getPakk(suvalineIndeks);
+            System.out.println(suvalineIndeks);
+            System.out.println(suvalineKaart);
+            // add kaart vastasele
+        }
+        return true;
+    }
 }
