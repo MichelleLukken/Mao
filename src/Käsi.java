7fe21e4fb85ef65030b3d92d09dfbe539e9e31f4
Anette Habanen <habanen@ut.ee>
2021-03-22 10:20:00 +0200

    Käsi.java

--- src/Käsi.java
+++ src/Käsi.java
@@ -1,8 +1,24 @@
+import java.util.ArrayList;
+
 public class Käsi {
-    public void võtaKaart(){
+
+    private ArrayList<Kaart> käes;
+
+    public boolean võtaKaart(Kaardipakk kaardipakk){
+        kaardipakk.suvalineKaart();
+        käes.add(kaardipakk.suvalineKaart());
+        return true;
 
     }
     public void käiKaart(){
 
+    }
+
+    public Käsi(ArrayList<Kaart> käes) {
+        this.käes = käes;
+    }
+
+    public ArrayList<Kaart> getKäes() {
+        return käes;
     }
 }
